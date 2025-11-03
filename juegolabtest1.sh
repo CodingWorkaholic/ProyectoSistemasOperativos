fdc1863e6ff53caf65e1b866a498d86b83cd554c
Workaholic of Codes <102420573+CodingWorkaholic@users.noreply.github.com>
2025-11-03 17:06:31 -0300

    Enhance maze game with quiz questions
    Added quiz questions to the maze game and modified game flow.

--- juegolabtest1.sh
+++ juegolabtest1.sh
@@ -1,6 +1,3 @@
-# Juego de Laberinto para Git Bash
-# Controles: w=arriba, s=abajo, a=izquierda, d=derecha, q=salir
-
 # Colores
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -11,6 +8,8 @@ NC='\033[0m' # Sin color
 
 asd=1
 nivel=1
+opcionPreg=0
+respuesta=""
 
 # Posición inicial del jugador
 player_x=1
@@ -24,6 +23,138 @@ goal_y=9
 moves=0
 
 # Laberinto (0=camino, 1=pared)
+preguntas(){
+ case $((1 + RANDOM % 7)) in
+ 1)
+ 	echo "¿Para qué sirven las ${GREEN}tuberías${NC} o ${GREEN}pipelines${NC}?"
+	echo "a) Conectar salidas de un comando con entradas del siguiente"
+	echo "b) Crear un script"
+	echo "c) Sobreescribir un archivo .txt"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "a" || $respuesta = "A" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+	
+    ;;
+  2)
+    	echo "¿Qué debe ir ${YELLOW}siempre${NC} al comenzar un script en ${GREEN}bash${NC}?"
+	echo "a) bin-bash:"
+	echo "b) script.sh"
+	echo "c) #!/bin/bash"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "c" || $respuesta = "C" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+  3)
+    	echo "¿Qué hace el comando base ${GREEN}wc${NC}?"
+	echo "a) Cambia, borra o comprime caracteres"
+	echo "b) Muestra las últimas líneas de un archivo"
+	echo "c) Cuenta la cantidad de líneas, palabras y carácteres que tiene un archivo"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "c" || $respuesta = "C" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+
+ 4)
+    	echo "¿En los operadores de comparación ${GREEN}-ge${NC} es…?"
+	echo "a) destino de"
+	echo "b) mayor que"
+	echo "c) mayor o igual que"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "c" || $respuesta = "C" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+
+ 5)
+    	echo "¿El comando ${GREEN}for${NC} es un…?"
+	echo "a) contador"
+	echo "b) acumulador"
+	echo "c) condición"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "b" || $respuesta = "B" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+
+ 6)
+    	echo "¿Qué es ${GREEN}||${NC}?"
+	echo "a) and"
+	echo "b) not"
+	echo "c) or"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "c" || $respuesta = "C" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+
+7)
+    	echo "¿Qué es un ${GREEN}Sistema Operativo${NC}?"
+	echo "a) Un sistema operativo es un programa que actúa como intermediario entre el usuario y el hardware de la computadora"
+	echo "b) Un sistema operativo es un administrador de tareas, archivos que ayudan al Usuario"
+	echo "c) Es un conjunto de aplicaciones que ayudan en el manejo del hardware a la hora de hacer tareas: como navegar en internet, editar textos o mirar videos"
+    	read -p "" respuesta
+
+	if [[ $respuesta = "a" || $respuesta = "A" ]]; then
+	echo "La respuesta es correcta, bien hecho!"
+	sleep 2
+	else
+	echo "La respuesta es incorrecta. Hazlo mejor para la próxima!"
+	sleep 2
+	clear_screen
+	exit 0
+	fi
+    ;;
+    
+  *)
+    echo "Opcion no valida"
+    ;;
+esac
+}
 
 tiponivel(){
 moves=0
@@ -192,8 +323,8 @@ game_loop() {
             echo ""
             read -p "Presiona ENTER para responder preguntas, tienes 1 sola oportunidad..."
 	clear_screen
-            read -p "Para que sirve el echo en la terminal?"
-		draw_maze
+	preguntas
+	draw_maze
 		
         fi
 
@@ -213,16 +344,16 @@ game_loop() {
 
 clear_screen
 echo "Bienvenido a ${CYAN}WinSurf${NC}"
-sleep 3
+#sleep 3
 clear_screen
 echo "Tu computadora ha sido ${RED}hackeada${NC}"
-sleep 3
+#sleep 3
 clear_screen
 echo "¡Pero todavía no está todo acabado!"
-sleep 3
+#sleep 3
 clear_screen
 echo "Resuelve ejercicios y ve ${GREEN}recuperando${NC} tus datos"
-sleep 3
+#sleep 3
 clear_screen
 echo "Estás listo? Presiona ${GREEN}ENTER${NC} para comenzar..."
 read -p ""
