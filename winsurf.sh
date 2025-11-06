50484935213af4c3544c3458c63245af79cca8f6
Workaholic of Codes <102420573+CodingWorkaholic@users.noreply.github.com>
2025-11-06 08:22:34 -0300

    Fix echo statements for user prompts in winsurf.sh

--- winsurf.sh
+++ winsurf.sh
@@ -321,7 +321,8 @@ game_loop() {
 	if [ $result -eq 2 ]; then
             echo -e "${RED}¡Haz impactado contra un virus!${NC}"
             echo ""
-            read -p "Presiona ${GREEN}ENTER${NC} para responder preguntas, tienes 1 sola oportunidad..."
+            echo "Presiona ${GREEN}ENTER${NC} para responder preguntas, tienes 1 sola oportunidad..."
+	    read -p ""
 	clear_screen
 	preguntas
 	draw_maze
@@ -332,7 +333,8 @@ game_loop() {
             echo -e "${GREEN}¡¡¡FELICIDADES!!! ¡Has completado el nivel $nivel!${NC}"
             echo -e "Movimientos totales: ${YELLOW}${moves}${NC}"
             echo ""
-            read -p "Presiona ${GREEN}ENTER${NC} para pasar al siguiente..."
+            echo "Presiona ${GREEN}ENTER${NC} para pasar al siguiente..."
+	    read -p ""
             ((nivel=nivel+1))
 		tiponivel
 		draw_maze
